c6d22dcd8aca6210a7e5737d8f95f2ac61a40af2
denismaciel <denispmaciel@gmail.com>
2017-09-06 17:03:51 +0200

    add file to clean the data with tidytext package

---
+++ dw_cleaning.R
@@ -0,0 +1,21 @@
+library(tidyverse)
+library(stringr)
+library(tidytext)
+library(tm)
+
+df_long <- to_be_cleaned %>% 
+  mutate(review_text = str_replace_all(review_text, "\\.", "\\. ")) %>% 
+  mutate(review_text = str_replace_all(review_text, "-", " ")) %>% 
+  unnest_tokens(word, review_text) %>% 
+  group_by(review_id) %>% 
+  mutate(word_order = row_number()) %>% 
+  ungroup() %>% 
+  anti_join(stop_words) %>% 
+  arrange(review_id, word_order) %>% 
+  mutate(word_stem = stemDocument(word, language = "english"))
+
+cleaned_out <- df_long %>% 
+  group_by(review_id, binary_rating, rating) %>% 
+  summarise(review_text = paste0(word_stem, collapse = " "))
+
+rm(df_long)
