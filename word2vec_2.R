745abc196273598da119598d0ac4bfe9c50f9d75
djordjedotlic <djordje.dotlic@yahoo.com>
2017-09-08 17:43:00 +0200

    word2vec features/ word2vec code adjusted

--- word2vec_2.R
+++ word2vec_2.R
@@ -1,115 +1,167 @@
-labeled <- train
+rm(list = ls(all = TRUE))
 
-train1 <- df$review_word2vec
-write(train1,"text_data.txt") 
-model=word2vec(train_file = "text_data.txt",output_file ="model1.bin",layer1_size = 300,min_count = 40,num_threads = 4,window = 10,sample = 0.001,binary=1)
-bin_to_txt("model1.bin","model1text.txt") 
-
-
+library(data.table)
+library(tidyverse)
+library(stringr)
+library(rword2vec)
 library(Rcpp)
 library(RcppArmadillo)
 library(tm)
-library(randomForest)
 library(rpart)
-library(rword2vec)
+
+
+to_be_cleaned <- as_tibble(readRDS("data_new/amazonBooks_train.RDS"))
+
+
+# CHOOSE
+ds_name <- "amazonBooks"
+# ds_name <- "amazonfinefood"
+#ds_name <- "imdb"
+# ds_name <- "twitter"
+
+# CHOOSE
+# type <- "test"
+type <- "train"
+
+# CHOOSE
+#size = 100000
+# size = 50000
+# size = 20000
+#size = 10000
+#size = 5000
+#size = 2500
+size = 1000
+
+
+technique <- "word2vec"
+file_name <- paste0("data_new/", ds_name, "_", type, ".RDS")
+
+to_log <- list(
+  technique = technique, 
+  file_name = file_name,
+  ds_name = ds_name,
+  type = type,
+  size = size
+)
+
+# CLEAN DATA ----
+start_cleaning <- Sys.time()
+source("dw_cleaning.R")
+end_cleaning <- Sys.time()
+df <- cleaned_out
+remove(cleaned_out)
+
+
+
+#Subsetting-----
+train_ids <- sample(1:nrow(df), size)
+df_sub <- df[train_ids,]
+
+sub_ids <- sample(1:nrow(df_sub), 0.8*nrow(df_sub))
+
+train <- df_sub[sub_ids,]
+test <- df_sub[-sub_ids,]
+
+
+
+train1 <- df_sub$review_text
+
+write(train1,"text_data.txt") 
+model <- word2vec(train_file = "text_data.txt",output_file ="model1.bin",layer1_size = 300,min_count = 40,num_threads = 4,window = 10,sample = 0.001,binary=1)
+bin_to_txt("model1.bin","model1text.txt") 
 
 
 
 sourceCpp("converter.cpp")
-vocab=as.data.frame(read.table("model1text.txt",header = F,stringsAsFactors = F,skip=1))
+vocab <- as.data.frame(read.table("model1text.txt",header = F,stringsAsFactors = F,skip=1))
+colnames(vocab)[1] <- "word"
 
+num_clusters <- floor(nrow(vocab)/10)
 
-colnames(vocab)[1]="word"
+vocab[is.na(vocab)] <- 0
 
-num_clusters=floor(nrow(vocab)/10)
-
-vocab <- vocab[-8356,]
-
-kmean_model= kmeans(vocab[,2:ncol(vocab)], centers = num_clusters, iter.max = 150)
+kmean_model <- kmeans(vocab[,2:ncol(vocab)], centers = num_clusters, iter.max = 150)
 write.csv(kmean_model$cluster, file="kmeans_clusters2.csv")
 write.csv(kmean_model$centers, file="kmeans_centers2.csv")
-kmean_model=NULL
+kmean_model <- NULL
 
-clusters=read.csv("kmeans_clusters2.csv")
-clusters$word=vocab$word
-colnames(clusters)=c("word_no","cluster","word")
+clusters <- read.csv("kmeans_clusters2.csv")
+clusters$word <- vocab$word
+colnames(clusters) <- c("word_no","cluster","word")
 
-### bag of centroid
-d=NULL
-d=strsplit(labeled$review_word2vec," ")
-for(i in 1:nrow(labeled))
+### bag of centroids for training data----
+start_features <- Sys.time()
+
+d <- NULL
+d <- strsplit(train$review_text," ")
+for(i in 1:nrow(train))
 {
-  d[[i]]=gsub("^\\s+|\\s+$", "",d[[i]])
+  d[[i]] <- gsub("^\\s+|\\s+$", "",d[[i]])
 }
 
-training_data= get_bag_of_centroids (d ,vocab$word,clusters$cluster,num_clusters)
-training_data=as.data.frame(training_data)
-training_data$rating=labeled$rating
+final <- get_bag_of_centroids (d ,vocab$word,clusters$cluster,num_clusters)
+end_features <- Sys.time()
 
-write.csv(training_data,"train_bag_of_centroids.csv",row.names = F)
+final <- as.data.frame(final)
+final$rating <- train$rating
 
-### test data
 
-d=NULL
-d=strsplit(test$review_word2vec," ")
+#Save thw train data-----
+saveRDS(final, paste0("./features/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
+
+#save training log file----
+to_log$computation_time <- list(
+  cleaning = list(end_cleaning = end_cleaning, 
+                  start_cleaning = start_cleaning,
+                  duration_cleaning = as.numeric(difftime(end_cleaning, start_cleaning, units = "secs"))),
+    features = list(end_features = end_features, 
+                  start_features = start_features,
+                  duration_features = as.numeric(end_features - start_features, units = "secs"))
+)
+to_log
+
+date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
+jsonlite::toJSON(to_log, pretty = T) %>% 
+  write(paste0("log_features/", to_log$technique, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
+
+
+### bag of centroids for test data----
+
+type <- "test"
+to_log <- list(
+  technique = technique, 
+  file_name = file_name,
+  ds_name = ds_name,
+  type = type,
+  size = size
+)
+
+start_features <- Sys.time()
+d <- NULL
+d <- strsplit(test$review_text," ")
 for(i in 1:nrow(test))
 {
-  d[[i]]=gsub("^\\s+|\\s+$", "",d[[i]])
+  d[[i]] <- gsub("^\\s+|\\s+$", "",d[[i]])
 }
-test_data=get_bag_of_centroids(d,vocab$word,clusters$cluster,num_clusters)
-test_data=as.data.frame(test_data)
-write.csv(test_data,"test_bag_of_centroids.csv",row.names = F)
+final <- get_bag_of_centroids(d,vocab$word,clusters$cluster,num_clusters)
+end_features <- Sys.time()
 
-training_data=read.csv("train_bag_of_centroids.csv")
-test_data=read.csv("test_bag_of_centroids.csv")
+final <- as.data.frame(final)
 
+#save test features----
+saveRDS(final, paste0("./features/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
 
+#save test log file----
+to_log$computation_time <- list(
+  cleaning = list(end_cleaning = end_cleaning, 
+                  start_cleaning = start_cleaning,
+                  duration_cleaning = as.numeric(difftime(end_cleaning, start_cleaning, units = "secs"))),
+    features = list(end_features = end_features, 
+                  start_features = start_features,
+                  duration_features = as.numeric(end_features - start_features, units = "secs"))
+)
+to_log
 
-
-feat <- training_data %>% rename(binary_rating = rating) 
-# colnames(training_data)
-# Make features a sparse matrix for the models
-feat_sparse <- Matrix::sparse.model.matrix(data = feat,
-                                           object = binary_rating ~ .-1)
-
-
-mod_glmnet  <-  cv.glmnet(x = training_data[,1:835],
-                          y = training_data$rating, 
-                          family = 'binomial', 
-                          alpha = 1,
-                          type.measure = "auc",
-                          nfolds = 5,
-                          thresh = 1e-3,
-                          maxit = 1e3)
-
-# model
-mod_xgboost <- xgboost(data = feat_sparse,
-                       label = feat$binary_rating,
-                       nrounds = 100,
-                       objective = "binary:logistic")
-
-# ============================= Predict on Test Set ===================================
-pred_test <- data_frame(pred_glmnet = predict(mod_glmnet, dtm_test, type = 'response')[, 1],
-                        pred_xgboost = predict(mod_xgboost, dtm_test, type = 'response'),
-                        actual = test$binary_rating)
-
-
-
-
-
-
-
-
-
-
-
-## randomForest
-ml_model=randomForest(as.factor(rating)~.,data=training_data[1:(round(0.8*nrow(training_data))),],importance=T,ntree=50)
-pred=predict(ml_model,training_data[(round(0.8*nrow(training_data))+1):nrow(training_data),])
-tab <- table(pred,training_data$rating[(round(0.8*nrow(training_data))+1):nrow(training_data)])
-pred_test=predict(ml_model,test_data)
-
-
-submit=data.frame(id=test$id,predicted_rating=pred_test)
-
-write.csv(submit,"rword2vec_boc_forest_solution.csv",row.names = F)
+date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
+jsonlite::toJSON(to_log, pretty = T) %>% 
+write(paste0("log_features/", to_log$technique, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
