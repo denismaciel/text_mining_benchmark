43c92bbde071e4ff5a2bf038484b703c7d443f96
denismaciel <denispmaciel@gmail.com>
2017-09-09 12:48:05 +0200

    add prediction on blind dataset; improve mod_framework with error hadnling

--- mod_framework.R
+++ mod_framework.R
@@ -10,8 +10,11 @@ library(xgboost)
 library(glmnet)
 # Pacakges to save the logs
 library(jsonlite)
-# ============================= Prepare the Data ===================================
-to_log <- list(file_name = "./data/LDA_features_AmazonBooks.rds",
+
+# ============= Determine the model parameters and features to be used  =====================
+# Every time the model is run, the results are logged to a text file for future reference.
+
+to_log <- list(file_path = "features/",
                params_xg = list(
                  nrounds = 60, 
                  objective = "binary:logistic",
@@ -33,7 +36,9 @@ to_log <- list(file_name = "./data/LDA_features_AmazonBooks.rds",
                user = Sys.info()['user']
 )
 
-df <- as_tibble(readRDS(to_log$file_name))
+# ============================= Prepare the Data ===================================
+df <- as_tibble(readRDS(to_log$file_path))
+if(!"binary_rating" %in% colnames(df)) stop("binary_rating is a necessary column but couldn't be found in the dataframe")
 
 label <- select(df, binary_rating)
 
@@ -80,19 +85,52 @@ pred_test <- data_frame(pred_glmnet = predict(mod_glmnet, test_feat, type = 'res
                         pred_xgboost = predict(mod_xgboost, test_feat, type = 'response'),
                         actual = test_label$binary_rating)
 
-
 source("./mod_assessment.R")
 
 # Include accuracy and AUC in the logs
-to_log$results <- list(accuracy, auc)
+to_log$results_test <- list(accuracy, auc)
 
+# ============================= Predict on Blind Set ===================================
+# From the file path of the train dataset, construct file path to the blind dataset
+beg <- str_locate(to_log$file_path, "features/")[, 2]
+end <- str_locate(to_log$file_path, "_train")[,1]
+stem <- str_sub(to_log$file_path, beg + 1 , end - 1)
+to_log$blind_path <- paste0("features/", stem, "_test_5000.RDS")
+
+blind <- readRDS(to_log$blind_path)
+
+if(!exists("blind")) stop(paste0("Error: path to blind dataset is invalid.",
+                                 "It could not be parsed from file_path"))
+                          
+blind <- select(df, -review_id, -rating) 
+colnames(blind)[-1] <- paste0("feat", colnames(blind)[-1])
+
+blind_sparse <- Matrix::sparse.model.matrix(data = blind,
+                                            object = binary_rating ~ .-1)
+
+pred_blind <- data_frame(pred_glmnet = predict(mod_glmnet, blind_sparse, type = 'response')[, 1],
+                         pred_xgboost = predict(mod_xgboost, blind_sparse, type = 'response'),
+                         actual = blind$binary_rating)
+
+pred_test <- pred_blind
+source("./mod_assessment.R")
+to_log$results_blind <- list(accuracy, auc)
+
+# ============================= Write Results to Log File ===================================
 toJSON(to_log, pretty = TRUE) %>% 
   write(file = paste0("log/", 
                       str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", ""),
                       "_",
-                      str_extract(to_log$file_name, "[A-Za-z]{1,10}_[A-Za-z]{1,20}_[A-Za-z0-9]{1,10}"),
+                      str_extract(to_log$file_path, "[A-Za-z0-9]{1,10}_[A-Za-z]{1,20}_[A-Za-z0-9]{1,10}"),
                       ".log"
   ))
 
-cat(cat(paste0(auc$model, " got an AUC of:\n\t", auc$auc), sep = "\n"),
-    cat(paste0(accuracy$model, " got an Accuracy of:\n\t", accuracy$acc_max), sep = "\n"))
+# ============================= Print Results to Screen ===================================
+cat(cat(paste0("TEST DATASET: ",to_log$results_test[[1]]$model, " got an Accuracy of:\n\t", to_log$results_test[[1]]$acc_max), sep = "\n"),
+    cat(paste0("TEST DATASET: ", to_log$results_test[[2]]$model, " got an AUC of:\n\t", to_log$results_test[[2]]$auc), sep = "\n"))
+
+cat(cat(paste0("BLIND DATASET: ",to_log$results_blind[[1]]$model, " got an Accuracy of:\n\t", to_log$results_blind[[1]]$acc_max), sep = "\n"),
+    cat(paste0("BLIND DATASET: ", to_log$results_blind[[2]]$model, " got an AUC of:\n\t", to_log$results_blind[[2]]$auc), sep = "\n"))
+
+# Clear workspace
+rm(list = ls())
