8c364f0aea90f831f17b5634967c9bf48f3dfe41
jaidikam <jaidikam@hu-berlin.de>
2017-09-14 20:27:09 +0200

    updated feature creation script

--- dw_dtm_lsa.R
+++ dw_dtm_lsa.R
@@ -1,26 +1,70 @@
-# CREATE CORPUS
-reviews_source <- VectorSource(cleaned_out$review_text)
-corpus <- VCorpus(reviews_source)
-rm(reviews_source)
-
-# Add 2- and 3grams function
-NgramTokenizer <-  function(x){
-  wordVec <- words(x)
-  bigramVec <- unlist(lapply(ngrams(wordVec, 2), paste, collapse = " "), use.names = FALSE)
-  # trigramVec <- unlist(lapply(ngrams(wordVec, 3), paste, collapse = " "), use.names = FALSE)
-  return(c(wordVec, bigramVec)) # add trigramVec to the sequence if used
-}
-
-# CREATE DTM and REDUCE SPARCIRY
-# When creating DTM exclude tokens that occur in less than 10 reviews
-tdm <- TermDocumentMatrix(corpus, control = list(bounds = list(global = c(10, Inf)),
-                                                 tokenize = NgramTokenizer))
-#tdm <- TermDocumentMatrix(corpus, control = list(bounds = list(global = c(1, Inf)),tokenize = NgramTokenizer,dictionary=rownames(tdm_train)))
 
 
+if(approach == "mix"){
+  # CREATE CORPUS
+  reviews_source <- VectorSource(cleaned_out$review_text)
+  corpus <- VCorpus(reviews_source)
+  rm(reviews_source)
+  
+  # Add 2- and 3grams function
+  NgramTokenizer <-  function(x){
+    wordVec <- words(x)
+    bigramVec <- unlist(lapply(ngrams(wordVec, 2), paste, collapse = " "), use.names = FALSE)
+    # trigramVec <- unlist(lapply(ngrams(wordVec, 3), paste, collapse = " "), use.names = FALSE)
+    return(c(wordVec, bigramVec)) # add trigramVec to the sequence if used
+  }
+  # CREATE DTM and REDUCE SPARCITY
+  # When creating DTM exclude tokens that occur in less than 10 reviews
+  tdm_train <- TermDocumentMatrix(corpus, control = list(bounds = list(global = c(10, Inf)),tokenize = NgramTokenizer)) 
+  rm(NgramTokenizer)
+  rm(corpus)
+  # Reduce the sparcity of the matrix
+  # dim(dtm)
+  tdm_train <- removeSparseTerms(tdm_train, sparse = 0.99)
+  }
 
-rm(NgramTokenizer)
-rm(corpus)
-# Reduce the sparcity of the matrix
-# dim(dtm)
-tdm <- removeSparseTerms(tdm, sparse = 0.99)
+
+
+
+
+
+
+if(approach == "foldin"){
+  
+  # CREATE CORPUS
+  reviews_source_train <- VectorSource(cleaned_out$review_text)
+  corpus_train <- VCorpus(reviews_source_train)
+  rm(reviews_source_train)
+  
+  
+ 
+ 
+  reviews_source_test <- VectorSource(test_data$review_text)
+  corpus_test <- VCorpus(reviews_source_test)
+  rm(reviews_source_test)
+  
+  # Add 2- and 3grams function
+  NgramTokenizer <-  function(x){
+    wordVec <- words(x)
+    bigramVec <- unlist(lapply(ngrams(wordVec, 2), paste, collapse = " "), use.names = FALSE)
+    # trigramVec <- unlist(lapply(ngrams(wordVec, 3), paste, collapse = " "), use.names = FALSE)
+    return(c(wordVec, bigramVec)) # add trigramVec to the sequence if used
+  }
+  
+  
+  tdm_train <- TermDocumentMatrix(corpus_train, control = list(bounds = list(global = c(10, Inf)),tokenize = NgramTokenizer)) 
+  tdm_train <- removeSparseTerms(tdm_train, sparse = 0.99)
+  tdm_test <- TermDocumentMatrix(corpus_test, control = list(bounds = list(global = c(10, Inf)),tokenize = NgramTokenizer,dictionary=rownames(tdm_train)))
+  #tdm_test <- removeSparseTerms(tdm_test, sparse = 0.99)
+  rm(NgramTokenizer)
+  rm(corpus_train)
+  rm(corpus_test)
+  # Reduce the sparcity of the matrix
+  # dim(dtm)
+  
+  
+  }
+
+
+
+
--- fe_LSA.R
+++ fe_LSA.R
@@ -1,5 +1,3 @@
-#REMEMBER TO SAVE  tdm_train <- tdm after first run if blind approach (fold in)
-#AND IN SECOND RUN : set dictionary=rownames(tdm_train) when creating TermDocumentMatrix for test set (in dw_dtm_lsa.R)
 
 ### PREPARATION ----
 # DOWNLOAD PACKAGES
@@ -28,17 +26,22 @@ type <- "train"
 # size = 100000
 # size = 50000
 # size = 20000
-# size = 10000
+ size = 10000
 # size = 5000
 # size = 2500
- size = 1000
+# size = 1000
 
+# CHOOSE
+#approach <- "mix"
+ approach <- "foldin" 
+ 
 
 technique <- "LSA"
 file_name <- paste0("data_new/", ds_name, "_", type, ".RDS")
 
 to_log <- list(
   technique = technique, 
+  approach = approach,
   file_name = file_name,
   ds_name = ds_name,
   type = type,
@@ -61,6 +64,17 @@ start_cleaning <- Sys.time()
 source("dw_cleaning.R")
 end_cleaning <- Sys.time()
 
+cleaned_out$type <- "train"
+
+test_name <- paste0("data_new/", ds_name, "_test_clean", ".RDS")
+test_data <- readRDS(test_name)
+
+# MERGE WITH 5K (FOR "MIX" APPROACH ONLY!)
+if(approach == "mix"){  
+  cleaned_out <- bind_rows(cleaned_out, test_data)
+  rm(test_data)
+}
+
 
 # CREATE TextDocumentMatrix ---
 start_tdm <- Sys.time()
@@ -72,28 +86,60 @@ end_tdm <- Sys.time()
 
 # ============================= Creating Features ===================================
 
-
+if(approach == "mix"){
 #create TermDocumentMatrix
 start_features <- Sys.time()
-myMatrix <- tdm
+myMatrix <- tdm_train
 myMatrix = as.matrix(myMatrix)
 
 #create latent semantic space
 myMatrix.lsa = lsa(myMatrix,dimcalc_share())
-myMatrix.lsa_tk=as.data.frame(myMatrix.lsa$tk)
-myMatrix.lsa_dk=as.data.frame(myMatrix.lsa$dk)
-myMatrix.lsa_sk=as.data.frame(myMatrix.lsa$sk)
+projectedmatrix = t(as.textmatrix(myMatrix.lsa))
+projectedmatrix <-matrix(projectedmatrix, nrow = dim(projectedmatrix)[1],
+                         ncol = dim(projectedmatrix)[2])
 
-#create features from discovered latent concepts
-cr_feature <- myMatrix.lsa_dk
-cr_feature$binary_rating <- cleaned_out$binary_rating
-cr_feature$rating <- cleaned_out$rating
-cr_feature$review_id <- cleaned_out$review_id
+projectedmatrix <- as.data.table(projectedmatrix)
+projectedmatrix$binary_rating <- cleaned_out$binary_rating
+projectedmatrix$rating <- cleaned_out$rating
+projectedmatrix$review_id <- cleaned_out$review_id
+projectedmatrix$type <- cleaned_out$type
+
 
 end_features <- Sys.time()
+}
+
+if(approach == "foldin"){
+  #create TermDocumentMatrix
+  start_features <- Sys.time()
+  myMatrix <- tdm_train
+  myMatrix = as.matrix(myMatrix)
+  #create latent semantic space
+  myMatrix.lsa = lsa(myMatrix,dimcalc_share())
+  projectedmatrix.train = t(as.textmatrix(myMatrix.lsa))
+  projectedmatrix.train <-matrix(projectedmatrix.train, nrow = dim(projectedmatrix.train)[1],
+                                  ncol = dim(projectedmatrix.train)[2])
+  projectedmatrix.train <- as.data.table(projectedmatrix.train)
+  projectedmatrix.train$binary_rating <- cleaned_out$binary_rating
+  projectedmatrix.train$rating <- cleaned_out$rating
+  projectedmatrix.train$review_id <- cleaned_out$review_id
+  projectedmatrix.train$type <- cleaned_out$type
+  
+  projected.test.lsa = fold_in(docvecs = as.matrix(tdm_test), LSAspace = myMatrix.lsa)
+  projectedmatrix.test <- t(projected.test.lsa)
+  projectedmatrix.test <-matrix(projectedmatrix.test, nrow = dim(projectedmatrix.test)[1],
+                                 ncol = dim(projectedmatrix.test)[2])
+  projectedmatrix.test <- as.data.table(projectedmatrix.test)
+  projectedmatrix.test$binary_rating <- test_data$binary_rating
+  projectedmatrix.test$rating <- test_data$rating
+  projectedmatrix.test$review_id <- test_data$review_id
+  projectedmatrix.test$type <- test_data$type
+  
+  projectedmatrix <- rbind(projectedmatrix.train,projectedmatrix.test)
+  end_features <- Sys.time()
+}
 
 # SAVE RESULTS----
-saveRDS(cr_feature, paste0("./features/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
+saveRDS(projectedmatrix, paste0("./features/", to_log$technique, "_", approach, "_", ds_name, "_", type, "_", size, ".RDS"))
 
 
 # SAVE LOG ----
@@ -112,4 +158,4 @@ to_log
 
 date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
 jsonlite::toJSON(to_log, pretty = T) %>% 
-  write(paste0("log_features/", to_log$technique, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
+  write(paste0("log_features/", to_log$technique, "_", approach, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
