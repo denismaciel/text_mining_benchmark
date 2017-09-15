f0a2fca1bbfe42b26730d0fc74e6a6e26a730e37
djordjedotlic <djordje.dotlic@yahoo.com>
2017-09-15 16:12:47 +0200

    updated vector averaging

--- vector averaging 2.R
+++ vector averaging 2.R
@@ -1,13 +1,23 @@
+library(data.table)
+library(tidyverse)
+library(stringr)
+library(rword2vec)
+library(Rcpp)
+library(RcppArmadillo)
+library(tm)
+library(rpart)
+
+for (i in 1:7){
 #vector averaging
 
-train <- readRDS("feats_dotlic/amazonBooks_clean_train")
-test <- readRDS("feats_dotlic/amazonBooks_clean_test")
+train <- readRDS("data_new/amazonfinefood_train.rds")
+test <- readRDS("data_new/amazonfinefood_test.rds")
 
 
 
 # CHOOSE
-ds_name <- "amazonBooks"
-##ds_name <- "amazonfinefood"
+#ds_name <- "amazonBooks"
+ds_name <- "amazonfinefood"
 #ds_name <- "imdb"
 #ds_name <- "twitter"
 #ds_name <- "yelp"
@@ -20,27 +30,19 @@ type <- "train"
 #size = 100000
 #size = 50000
 #size = 20000
-size = 10000
+#size = 10000
 #size = 5000
 #size = 2500
 #size = 1000
 
-for (i in 5:7){
-  
-  ds_name <- "yelp"
-  type <- "train"
-  list_cuts <- as.list(c(1000,2500,5000,10000,20000,50000,100000))
-  
-  
-  train <- readRDS("data_new/yelp_train.RDS")
-  test <- readRDS("data_new/yelp_test.RDS")
-  
-  
-  size <- list_cuts[[i]]
+list_cuts <- as.list(c(1000,2500,5000,10000,20000,50000, 100000))
+size <- list_cuts[[i]]
 
 
 
-technique <- "word2vec_vector_averaging"
+
+
+technique <- "word2vecVectorAveraging"
 file_name <- paste0("data_new/", ds_name, "_", type, ".RDS")
 
 to_log <- list(
@@ -63,16 +65,16 @@ train <- df_sub[sub_ids,]
 
 df <- data.frame(row.names = (1:(nrow(train)+nrow(test))))
 df$review_id <- NA
-df$binary_rating <- NA
 df$rating <- NA
+df$binary_rating <- NA
 df$review_text <- NA
 df[1:nrow(train),] <- train 
 df[(nrow(train)+1):(nrow(train)+nrow(test)),] <- test
 
 to_be_cleaned <- data.frame(row.names = (1:(nrow(train)+nrow(test))))
 to_be_cleaned$review_id <- NA
-to_be_cleaned$binary_rating <- NA
 to_be_cleaned$rating <- NA
+to_be_cleaned$binary_rating <- NA
 to_be_cleaned$review_text <- NA
 to_be_cleaned[1:nrow(train),] <- train 
 to_be_cleaned[(nrow(train)+1):(nrow(train)+nrow(test)),] <- test
@@ -80,20 +82,22 @@ to_be_cleaned[(nrow(train)+1):(nrow(train)+nrow(test)),] <- test
 test_dummy <- data.frame(row.names = (1:nrow(to_be_cleaned)))
 test_dummy$review_id <- NA
 test_dummy$review_id <- to_be_cleaned$review_id
-test_dummy$test <- NA
-test_dummy$test[1:size] <- 0
-test_dummy$test[(size+1):nrow(test_dummy)] <- 1
+test_dummy$type <- NA
+test_dummy$type[1:size] <- 0
+test_dummy$type[(size+1):nrow(test_dummy)] <- 1
 
 source("dw_cleaning.R")
 df <- cleaned_out
 
 df <- merge.data.frame(df,test_dummy, by = "review_id")
+df$type <- ifelse(df$type == "0","train", "test")
+
 
 remove(cleaned_out, df_sub, test_dummy)
 
-df_int <- split ( df,df$test)
-train <- df_int$`0`
-test <- df_int$`1`
+df_int <- split ( df,df$type)
+train <- df_int$train
+test <- df_int$test
 
 remove(df_int)
 
@@ -108,35 +112,34 @@ sourceCpp("converter.cpp")
 vocab <- as.data.frame(read.table("model1text.txt",header = F,stringsAsFactors = F,skip=1))
 vocab[is.na(vocab)] <- 0
 colnames(vocab)[1]="word"
-print(str(vocab))
 
 #Mixed approach----
 ### From Words to Paragraphs, Attempt 1:Vector Averaging 
 start_features <- Sys.time()
 d=NULL
 d=strsplit(df$review_text," ")
-for(i in 1:nrow(df))
+for(j in 1:nrow(df))
 {
-  d[[i]]=gsub("^\\s+|\\s+$", "",d[[i]])
+  d[[j]]=gsub("^\\s+|\\s+$", "",d[[j]])
 }
+
+df$rownames <- as.character(row.names(df))
+df$rownames <- as.numeric(df$rownames)
 
 df_data <- get_average_vectors(d,matrix(as.numeric(unlist(vocab[,2:ncol(vocab)])),nrow=nrow(vocab)),vocab$word,stopwords())
 end_features <- Sys.time()
 
 df_data <- as.data.frame(df_data)
-df_data$rating <- NA
-df_data$rating[1:nrow(train)] <- train$binary_rating
-df_data$rating [(nrow(train)+1):nrow(df_data)] <- test$binary_rating
-
-
-#train <- df_data[1:size,]
-#test <- df_data[(size + 1):nrow(df_data),]
+df_data$rownames <- as.character(row.names(df_data))
+df_data$rownames <- as.numeric(df_data$rownames)
+df_data <- merge.data.frame(df_data,df, by = "rownames")
+df_data$review_text <- NULL
+df_data$rownames <- NULL
 
 
 #Save thw train data
-saveRDS(df_data, paste0("./features/mixed/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
-#type <- "test"
-#saveRDS(test, paste0("./features/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
+saveRDS(df_data, paste0("./features/", to_log$technique, "_", "mix_" ,ds_name, "_", type, "_", size, ".RDS"))
+
 
 #save training log file
 type <- "train"
@@ -153,32 +156,35 @@ to_log
 
 date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
 jsonlite::toJSON(to_log, pretty = T) %>% 
-  write(paste0("log_features/mixed/", to_log$technique, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
+write(paste0("log_features/", to_log$technique, "_", "mix_" ,ds_name, "_", type, "_", size, "_", date, ".log"))
 
-#mixed approach----
+#blind approach----
 
 start_features <- Sys.time()
 d=NULL
 d=strsplit(train$review_text," ")
-for(i in 1:nrow(train))
+for(k in 1:nrow(train))
 {
-  d[[i]]=gsub("^\\s+|\\s+$", "",d[[i]])
+  d[[k]]=gsub("^\\s+|\\s+$", "",d[[k]])
 }
+
+train$rownames <- as.character(row.names(train))
+train$rownames <- as.numeric(train$rownames)
 
 df_data <- get_average_vectors(d,matrix(as.numeric(unlist(vocab[,2:ncol(vocab)])),nrow=nrow(vocab)),vocab$word,stopwords())
 end_features <- Sys.time()
 
 df_data <- as.data.frame(df_data)
-df_data$rating <- NA
-df_data$rating[1:nrow(train)] <- train$binary_rating
-
-
-#train <- df_data[1:size,]
-#test <- df_data[(size + 1):nrow(df_data),]
+df_data$rownames <- as.character(row.names(df_data))
+df_data$rownames <- as.numeric(df_data$rownames)
+df_data <- merge.data.frame(df_data,train, by = "rownames")
+df_data$review_text <- NULL
+df_data$rownames <- NULL
+df_data <- as.data.frame(df_data)
 
 
 #Save thw train data
-saveRDS(df_data, paste0("./features/blind/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
+saveRDS(df_data, paste0("./features/", to_log$technique, "_", "blind_" ,ds_name, "_", type, "_", size, ".RDS"))
 #type <- "test"
 #saveRDS(test, paste0("./features/", to_log$technique, "_", ds_name, "_", type, "_", size, ".RDS"))
 
@@ -197,7 +203,7 @@ to_log
 
 date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
 jsonlite::toJSON(to_log, pretty = T) %>% 
-  write(paste0("log_features/blind/", to_log$technique, "_", ds_name, "_", type, "_", size, "_", date, ".log"))
+  write(paste0("log_features/", to_log$technique, "_", "blind_", ds_name, "_", type, "_", size, "_", date, ".log"))
 
 
 rm(list = ls(all = TRUE))
