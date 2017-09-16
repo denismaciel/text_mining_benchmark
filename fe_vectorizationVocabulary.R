b87a6baf691b142e3d510c6c5d602614d5f65dfa
denismaciel <denispmaciel@gmail.com>
2017-09-16 23:36:41 +0200

    put old fe_vectorizationVOcabulary to zz files; add new fe_vectorizationVocabulary compatible with modelling approach

--- fe_vectorizationVocabulary.R
+++ fe_vectorizationVocabulary.R
@@ -1,231 +1,105 @@
-if(!require("data.table")) install.packages("data.table"); library("data.table")
-if(!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
-if(!require("stringr")) install.packages("stringr"); library("stringr")
-if(!require("Matrix")) install.packages("Matrix"); library("Matrix")
+#============================= Extra Packages ===================================
+if(!require("rword2vec")) install.packages("rword2vec"); library("rword2vec")
 if(!require("text2vec")) install.packages("text2vec"); library("text2vec")
 
+### PREPARATION ----
+# DOWNLOAD PACKAGES
+if(!require("tm")) install.packages("tm"); library(tm)
+if(!require("tidytext")) install.packages("tidytext"); library(tidytext)
+if(!require("data.table")) install.packages("data.table"); library(data.table)
 
-for (i in 1:7){
+to_log <- list(
+  technique = technique,
+  file_name = file_name,
+  ds_name = ds_name,
+  size = size,
+  path_to_output = path_to_output
+)
+
+# DOWNLOAD DATA ----
+raw_data <- readRDS(file_name)
+
+# SPLIT DATA ----
+ind_sample <- sample(1:nrow(raw_data), size = to_log$size)
+to_be_cleaned <- raw_data[ind_sample, ]
+rm(raw_data)
+
+# CLEAN DATA ----
+start_cleaning <- Sys.time()
+source("dw_cleaning.R")
+end_cleaning <- Sys.time()
+
+cleaned_out$type <- "train"
+
+# MERGE WITH 5K (FOR "MIX" APPROACH ONLY!)
+test_name <- paste0("data_new/", ds_name, "_test_clean", ".RDS")
+test_data <- readRDS(test_name)
+cleaned_out <- bind_rows(cleaned_out, test_data)
+rm(test_data)
+
+cleaned_out
+# ============================= Blind Approach ===================================
+blind_train <- cleaned_out %>% filter(type == "train")
+blind_test <- cleaned_out %>% filter(type == "test")
+mix <- cleaned_out
+
+create_vectorVocabulary_features <- function(df) {
+
+  setDT(df)
+  setkey(df, review_id)
   
-  train <- readRDS("data_new/imdb_train.RDS")
-  test <- readRDS("data_new/imdb_test.RDS")
-  
-  #ds_name <- "amazonBooks"
-  #ds_name <- "amazonfinefood"
-  ds_name <- "imdb"
-  #ds_name <- "twitter"
-  #ds_name <- "yelp" 
-  type <- "train"
-  list_cuts <- as.list(c(1000,2500,5000,10000,20000,50000, 100000))
-  
-  size <- list_cuts[[i]]
-  
-  
-  technique <- "vectorizationVocabulary"
-  file_name <- paste0("data_new/", ds_name, "_", type, ".RDS")
-  
-  to_log <- list(
-    technique = technique, 
-    file_name = file_name,
-    ds_name = ds_name,
-    type = type,
-    size = size
-  )
-  
-  #subsetting
-  start_cleaning <- Sys.time()
-  train_ids <- sample(1:nrow(train), size)
-  df_sub <- train[train_ids,]
-  train <- df_sub
-  
-  to_be_cleaned <- data.frame(row.names = (1:(nrow(train)+nrow(test))))
-  to_be_cleaned$review_id <- NA
-  to_be_cleaned$rating <- NA
-  to_be_cleaned$binary_rating <- NA
-  to_be_cleaned$review_text <- NA
-  
-  to_be_cleaned[1:nrow(train),] <- train
-  to_be_cleaned[(nrow(train) + 1):(nrow(train)+nrow(test)),] <- test
-  
-  
-  test_dummy <- data.frame(row.names = (1:nrow(to_be_cleaned)))
-  test_dummy$review_id <- NA
-  test_dummy$review_id <- to_be_cleaned$review_id
-  test_dummy$type <- NA
-  test_dummy$type[1:size] <- 0
-  test_dummy$type[(size+1):nrow(test_dummy)] <- 1
-  
-  
-  source("dw_cleaning.R")
-  df <- cleaned_out
-  
-  
-  
-  df <- merge.data.frame(df,test_dummy, by = "review_id")
-  df$type <- ifelse(df$type == 0,"train", "test")
-  
-  remove(cleaned_out, df_sub, test_dummy)
-  
-  df_int <- split ( df,df$type)
-  train <- df_int$train
-  test <- df_int$test
-  
-  remove(df_int)
-  end_cleaning <- Sys.time()
-  
-  
-  #Mixed approach----
-  # Transform train
-  start_features <- Sys.time()
-  tok_fun <- word_tokenizer
   it_train <- itoken(df$review_text, 
-                     tokenizer = tok_fun, 
+                     tokenizer = word_tokenizer, 
                      ids = df$review_id, 
                      progressbar = TRUE)
+  
   vocab <- create_vocabulary(it_train, ngram = c(1L, 2L))
+  
   pruned_vocab <- prune_vocabulary(vocab, 
                                    term_count_min = 10, 
                                    doc_proportion_max = 0.5,
                                    doc_proportion_min = 0.001)
   vectorizer <- vocab_vectorizer(pruned_vocab)
-  
   dtm <- create_dtm(it_train, vectorizer)
-  dtm <- normalize(dtm, "l1") #normalizes dtm_train so that every row sums up to 1
+  #normalize so that every row sums up to 1
+  dtm <- normalize(dtm, "l1") 
+  return(dtm)
+}
+
+blind_train_featsparse <- create_vectorVocabulary_features(blind_train)
+blind_test_featsparse <- create_vectorVocabulary_features(blind_test)
+mix_featsparse <- create_vectorVocabulary_features(mix)
+
+# ============================= Split Mix after feature creation ================
+mix_test <- cleaned_out %>% filter(type == "test")
+mix_train <- cleaned_out %>% filter(type == "train")
+
+ind <- which(rownames(mix_featsparse) %in% mix_test$review_id)
+mix_test_featsparse <- mix_featsparse[ind, ]
+
+ind <- which(rownames(mix_featsparse) %in% mix_train$review_id)
+mix_train_featsparse <- mix_featsparse[ind, ]
+
+# ============================= Save to files ===================================
+
+save_output <- function(df, approach, tt, feat_or_label){
   
-  end_features <- Sys.time()
-  
-  #Save thw train data
-  saveRDS(dtm, paste0("./features/", to_log$technique, "_", "mix_" ,ds_name, "_", type, "_", size, ".RDS"))
-  
-  #save mixed log file
-  to_log$computation_time <- list(
-    cleaning = list(end_cleaning = end_cleaning, 
-                    start_cleaning = start_cleaning,
-                    duration_cleaning = as.numeric(difftime(end_cleaning, start_cleaning, units = "secs"))),
-    features = list(end_features = end_features, 
-                    start_features = start_features,
-                    duration_features = as.numeric(end_features - start_features, units = "secs"))
-  )
-  to_log
-  
-  date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
-  jsonlite::toJSON(to_log, pretty = T) %>% 
-    write(paste0("log_features/", to_log$technique, "_", "mix_" ,ds_name, "_", type, "_", size, "_", date, ".log"))
-  
-  remove(df,dtm, vocab, pruned_vocab)
-  
-  #Blind approach----
-  # Transform train and test
-  start_features <- Sys.time()
-  tok_fun <- word_tokenizer
-  it_train <- itoken(train$review_text, 
-                     tokenizer = tok_fun, 
-                     ids = train$review_id, 
-                     progressbar = TRUE)
-  vocab <- create_vocabulary(it_train, ngram = c(1L, 2L))
-  pruned_vocab <- prune_vocabulary(vocab, 
-                                   term_count_min = 10, 
-                                   doc_proportion_max = 0.5,
-                                   doc_proportion_min = 0.001)
-  
-  vectorizer <- vocab_vectorizer(pruned_vocab)
-  
-  dtm_train <- create_dtm(it_train, vectorizer)
-  dtm_train <- normalize(dtm_train, "l1") #normalizes dtm_train so that every row sums up to 1
-  end_features <- Sys.time()
-  
-  
-  #Save thw train data
-  saveRDS(dtm_train, paste0("./features/", to_log$technique, "_", "blind_", ds_name, "_", type, "_", size, ".RDS"))
-  
-  #save training log file
-  to_log$computation_time <- list(
-    cleaning = list(end_cleaning = end_cleaning, 
-                    start_cleaning = start_cleaning,
-                    duration_cleaning = as.numeric(difftime(end_cleaning, start_cleaning, units = "secs"))),
-    features = list(end_features = end_features, 
-                    start_features = start_features,
-                    duration_features = as.numeric(end_features - start_features, units = "secs"))
-  )
-  to_log
-  
-  date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
-  jsonlite::toJSON(to_log, pretty = T) %>% 
-    write(paste0("log_features/", to_log$technique, "_", "blind_", ds_name, "_", type, "_", size, "_", date, ".log"))
-  
-  
-  rm(list = ls(all = TRUE))
+  saveRDS(df,
+          paste0(to_log$path_to_output,
+                 paste(to_log$technique, approach, to_log$ds_name, tt, to_log$size, feat_or_label, sep = "_"),
+                 ".RDS"))
   
 }
 
-#blind test
+# Blind
+save_output(blind_test_featsparse, "blind", "test", "feat")
+save_output(blind_train_featsparse, "blind", "train", "feat")
+save_output(blind_train, "blind", "train", "label")
+save_output(blind_test, "blind", "test", "label")
+# Mix
+save_output(mix_test_featsparse, "mix", "test", "feat")
+save_output(mix_train_featsparse, "mix", "train", "feat")
+save_output(mix_train, "mix", "train", "label")
+save_output(mix_test, "mix", "test", "label")
 
-
-
-test <- readRDS("data_new/yelp_test.RDS")
-#ds_name <- "amazonBooks"
-#ds_name <- "amazonfinefood"
-#ds_name <- "imdb"
-#ds_name <- "twitter"
-ds_name <- "yelp" 
-
-type <- "test"
-size <- 5000
-
-technique <- "vectorizationVocabulary"
-file_name <- paste0("data_new/", ds_name, "_", type, ".RDS")
-
-to_log <- list(
-  technique = technique, 
-  file_name = file_name,
-  ds_name = ds_name,
-  type = type,
-  size = size
-)
-start_cleaning <- Sys.time()
-to_be_cleaned <- test
-
-
-source("dw_cleaning.R")
-test <- cleaned_out
-remove(cleaned_out)
-end_cleaning <- Sys.time()
-
-start_features <- Sys.time()
-tok_fun <- word_tokenizer
-it_train <- itoken(test$review_text, 
-                   tokenizer = tok_fun, 
-                   ids = test$review_id, 
-                   progressbar = TRUE)
-vocab <- create_vocabulary(it_train, ngram = c(1L, 2L))
-pruned_vocab <- prune_vocabulary(vocab, 
-                                 term_count_min = 10, 
-                                 doc_proportion_max = 0.5,
-                                 doc_proportion_min = 0.001)
-
-vectorizer <- vocab_vectorizer(pruned_vocab)
-
-dtm_train <- create_dtm(it_train, vectorizer)
-dtm_train <- normalize(dtm_train, "l1") #normalizes dtm_train so that every row sums up to 1
-end_features <- Sys.time()
-
-
-#Save thw test
-saveRDS(dtm_train, paste0("./features/", to_log$technique, "_", "blind_" ,ds_name, "_", type, "_", size, ".RDS"))
-
-#save test log file
-to_log$computation_time <- list(
-  cleaning = list(end_cleaning = end_cleaning, 
-                  start_cleaning = start_cleaning,
-                  duration_cleaning = as.numeric(difftime(end_cleaning, start_cleaning, units = "secs"))),
-  features = list(end_features = end_features, 
-                  start_features = start_features,
-                  duration_features = as.numeric(end_features - start_features, units = "secs"))
-)
-to_log
-
-date <- str_replace_all(strftime(Sys.time() , "%Y-%m-%dT%H:%M:%S"), ":", "")
-jsonlite::toJSON(to_log, pretty = T) %>% 
-  write(paste0("log_features/", to_log$technique, "_", "blind_", ds_name, "_", type, "_", size, "_", date, ".log"))
-
-
+rm(list = setdiff(ls(), c("loop_grid", "path_to_output", "METHOD")))
